a3dee05273f386484bed63e78a4b3b2e4277c703
caowenlong <caowenlong92@gmail.com>
2019-09-17 19:52:12 +0800

    add test sample scripts

--- test_samples.sh
+++ test_samples.sh
@@ -1,7 +1,28 @@
 #!/bin/sh
 
-samples=(`find build  -name "test_sample_*"`) # 将找到的结果保存为一个数组需要加 ()   
+# samples=$(find build  -name "test_sample_*") # 将找到的结果保存为一个数组需要加 ()
+cur_dir=$(pwd)
+cd build
+echo "## start test gen_file_md5"
+./test_get_file_md5
 
-for sample in $samples; do
-    ./${sample}
-done
+
+echo "## start test list_files_in_dir"
+./test_list_files_in_dir ${cur_dir}/samples ./ .cpp
+
+echo "## start test_sample_zlib"
+./test_sample_zlib
+
+# echo "## start test_sample_fstream"
+# ./test_sample_fstream
+
+echo "## start test_sample_eigen"
+./test_sample_eigen
+
+echo "## start test_sample_curl"
+./test_sample_curl
+
+echo "## start test_gen_json"
+./test_gen_json
+
+cd -
